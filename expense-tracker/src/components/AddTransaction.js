212133cb29a04dc67966c482555d91fa945da3df
iqranaz240 <iqranaz240@gmail.com>
2022-10-16 11:45:52 +0500

    updated app 2

--- expense-tracker/src/components/AddTransaction.js
+++ expense-tracker/src/components/AddTransaction.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function AddTransaction() {
+    const [description, setDescription] = useState('')
+    const [transactionAmount, setTransactionAmount] = useState(0)
+
   return (
     <div>
         <h3>Add New Transaction</h3>
         <form>
             <div className='form-control'>
                 <label htmlFor='description'>Description</label>
-                <input type='text'/>
+                <input type='text' id='description' value={description} onChange={(e)=>setDescription(e.target.value)}/>
             </div>
             <div className='form-control'>
                 <label htmlFor='transactionAmount'>Transaction Amount</label>
-                <input type='number'/>
+                <input type='number' id='transactionAmount' value={transactionAmount} onChange={(e)=>setTransactionAmount(e.target.value)}/>
             </div>
             <button className='btn'>Add Transaction</button>
         </form>
